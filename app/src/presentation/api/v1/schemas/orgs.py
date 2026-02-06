3a6bd5bc7c15e835c56bcda25fc3fbf25d8c0481
aleksandr <a24923093@gmail.com>
2026-02-06 13:15:49 +0700

    feat: added get_in_square endpoint

--- app/src/presentation/api/v1/schemas/orgs.py
+++ app/src/presentation/api/v1/schemas/orgs.py
@@ -1,10 +1,21 @@
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from pydantic_extra_types.phone_numbers import PhoneNumber
 
 from presentation.api.v1.schemas.activities import ActivityResponse
 from presentation.api.v1.schemas.buildings import BuildingResponse
+
+
+class GeoSearch(BaseModel):
+    lat: float = Field(..., ge=-180, le=180, description="Latitude")
+    lon: float = Field(..., ge=-180, le=180, description="Longitude")
+    distance: int = Field(
+        ...,
+        gt=0,
+        le=100_000_000,
+        description="Search area in **meters**",
+    )
 
 
 class OrganizationPhone(BaseModel):
@@ -23,7 +34,9 @@ class OrganizationResponseDetail(OrganizationResponse):
     phone_numbers: list[OrganizationPhone]
 
 
-class RadiusSearch(BaseModel):
-    lat: float
-    lon: float
-    radius: int
+class RadiusSearch(GeoSearch):
+    pass
+
+
+class SquareSearch(GeoSearch):
+    pass
